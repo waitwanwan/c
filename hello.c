4317e9d0267fd64f7c90478cd1c3898ab69e7ed3
wait <88075580+waitwan@users.noreply.github.com>
2021-07-28 14:25:06 +0800

    Update hello.c
    <=9 change to <9

--- hello.c
+++ hello.c
@@ -2,8 +2,8 @@
 void main(){
 
     int i,j;
-    for(i=1;i<=9;i++)
-        for(j=1;j<=9;j++)
+    for(i=1;i<9;i++)
+        for(j=1;j<9;j++)
         printf("%d * %d = %d \n",i,j,i*j);
     return 0;
 
